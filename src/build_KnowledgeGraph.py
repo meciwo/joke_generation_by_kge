8b47883130ce31a1b9c88ffa837ac007acc49da2
meciwo <shanshan0474@gmail.com>
2021-08-27 18:37:46 +0900

    fix openie

--- src/build_KnowledgeGraph.py
+++ src/build_KnowledgeGraph.py
@@ -1,6 +1,6 @@
 from tqdm import tqdm
 import pandas as pd
-from openie import StanfordOpenIE
+from openie import MyOpenIE
 import configparser
 
 config = configparser.ConfigParser()
@@ -10,12 +10,20 @@ kg_path = config["Paths"]["KnowlegeGraphPath"]
 
 data = pd.read_csv(jokedata_path, encoding="utf-8")
 
-KG = []
+kg = []
 
-with StanfordOpenIE() as client:
+with MyOpenIE() as client:
     for joke in tqdm(data["Joke"]):
-        for triple in client.annotate(joke.lower()):
-            KG.append([triple["subject"], triple["relation"], triple["object"]])
+        ann = client.annotate(
+            text=joke.lower(),
+            properties={
+                "openie.max_entailments_per_clause": 100,
+                "openie.triple.strict": True,
+            },
+            simple_format=True,
+        )
+        for triple in ann:
+            kg.append([triple["subject"], triple["relation"], triple["object"]])
 
-kg_df = pd.DataFrame(KG, columns=["head", "relation", "tail"])
+kg_df = pd.DataFrame(kg, columns=["head", "relation", "tail"]).drop_duplicates()
 kg_df.to_csv(kg_path)
