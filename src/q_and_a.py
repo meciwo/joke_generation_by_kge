b7a2b305bf40644cd4aa4c2ed63b5991b638464b
meciwo <shanshan0474@gmail.com>
2021-12-24 01:31:11 +0900

    fix id2idx

--- src/q_and_a.py
+++ src/q_and_a.py
@@ -16,45 +16,61 @@ def main():
     model_path = config["Paths"]["ModelPath"]
     ent_vocab_path = config["Paths"]["EntVocabPath"]
     rel_vocab_path = config["Paths"]["RelVocabPath"]
+    ent_id2idx_path = config["Paths"]["Entid2idxPath"]
+    rel_id2idx_path = config["Paths"]["Relid2idxPath"]
     date = config["Paths"]["Date"]
+    use_wiki = eval(config["Settings"]["use_wiki"])
 
-    model = torch.load(model_path + date + ".pkl", map_location="cpu")  # 読み出し
+    model_path = (
+        model_path + date + "_wiki.pkl" if use_wiki else model_path + date + ".pkl"
+    )
+    model = torch.load(model_path, map_location="cpu")  # 読み出し
+
     with open(ent_vocab_path, "rb") as f:
         ent_vocab = pickle.load(f)
     with open(rel_vocab_path, "rb") as f:
         rel_vocab = pickle.load(f)
+    with open(ent_id2idx_path, "rb") as f:
+        ent_id2idx = pickle.load(f)
+    with open(rel_id2idx_path, "rb") as f:
+        rel_id2idx = pickle.load(f)
 
     while True:
         try:
             print("please input your question:")
-            input_question = input()
-            # input_question = "what does horse eat?"
+            # input_question = input()
+            input_question = "what does horse eat?"
             sentence = list(sentencize(input_question.lower()))[0]
             print("Input:", sentence)
             head_w, tail_w = get_entities(sentence.text)
-            relation_w = get_relation(sentence.text)
-            print(f"head: {head_w}, tail: {tail_w}, relation: {relation_w}")
-            head, tail, relation = (
+            rel_w = get_relation(sentence.text)
+            print(f"head: {head_w}, tail: {tail_w}, relation: {rel_w}")
+            head_id, tail_id, rel_id = (
                 ent_vocab[head_w],
                 ent_vocab[tail_w],
-                rel_vocab[relation_w],
+                rel_vocab[rel_w],
             )
-            print(f"head:{head}, tail: {tail}, relation: {relation}")
+            head_idx, tail_idx, rel_idx = (
+                ent_id2idx[head_id],
+                ent_id2idx[tail_id],
+                rel_id2idx[rel_id],
+            )
+            # print(f"head:{head}, tail: {tail}, relation: {relation}")
 
             # 疑問詞を特定
 
             if cuda.is_available():
                 model.cuda()
-            triple = torch.tensor([[head], [tail], [relation]])
+            triple = torch.tensor([[head_idx], [tail_idx], [rel_idx]])
 
             # Link prediction evaluation on test set.
             evaluator = AnserPredictor(model, triple)
 
-            if 2 <= head <= 7:
+            if 2 <= head_id <= 7:
                 target = "head"
-            elif 2 <= tail <= 7:
+            elif 2 <= tail_id <= 7:
                 target = "tail"
-            elif 2 <= relation <= 7:
+            elif 2 <= rel_id <= 7:
                 target = "relation"
             else:
                 raise NotImplementedError
@@ -68,17 +84,18 @@ def main():
 
     evaluator.evaluate(b_size=1)
     topk_answers = evaluator.predict(pred_obj=target, topk=10)
-    ent_word = list(ent_vocab.keys())
-    rel_word = list(rel_vocab.keys())
-    print(len(ent_word))
-    print(len(rel_word))
-    for i, answer in enumerate(topk_answers):
-        if target == "head" or target == "tail":
-            print(i, ent_word[answer])
-        else:
-            print(i, rel_word[answer])
+    ent_word_list = list(ent_vocab.keys())
+    rel_word_list = list(rel_vocab.keys())
+    ent_id_list = [id for id, idx in sorted(ent_id2idx.items(), key=lambda x: x[1])]
+    rel_id_list = [id for id, idx in sorted(rel_id2idx.items(), key=lambda x: x[1])]
 
-    visualize(head_w, relation_w, tail_w)
+    for i, answer_idx in enumerate(topk_answers):
+        if target == "head" or target == "tail":
+            print(i, ent_word_list[ent_id_list[answer_idx]])
+        else:
+            print(i, rel_word_list[rel_id_list[answer_idx]])
+
+    # visualize(head_w, rel_w, tail_w)
 
 
 if __name__ == "__main__":
--- src/word2id.py
+++ src/word2id.py
@@ -2,12 +2,16 @@ import pandas as pd
 import pickle
 import configparser
 
+from utils.datasets import load_joke_dataset
+
 config = configparser.ConfigParser()
 config.read("config.ini")
 kg_path = config["Paths"]["KnowlegeGraphPath"]
 train_path = config["Paths"]["TrainDataPath"]
 ent_vocab_path = config["Paths"]["EntVocabPath"]
 rel_vocab_path = config["Paths"]["RelVocabPath"]
+ent_id2idx_path = config["Paths"]["Entid2idxPath"]
+rel_id2idx_path = config["Paths"]["Relid2idxPath"]
 
 ke = pd.read_csv(kg_path)
 ent_word2id, rel_word2id = {}, {}
@@ -32,7 +36,7 @@ for word in list(ke["head"].values) + list(ke["tail"].values):
         cnt += 1
 
 cnt = 8
-for i, word in enumerate(set(list(ke["relation"].values)), start=cnt):
+for word in ke["relation"].values:
     if word not in rel_word2id:
         rel_word2id[word] = cnt
         cnt += 1
@@ -58,3 +62,13 @@ with open(ent_vocab_path, "wb") as f:
 
 with open(rel_vocab_path, "wb") as f:
     pickle.dump(rel_word2id, f)  # 保存
+
+kg_train, _, _ = load_joke_dataset("./data", valid_size=100, test_size=100)
+
+ent_id2idx, rel_id2idx = kg_train.ent2ix, kg_train.rel2ix
+
+with open(ent_id2idx_path, "wb") as f:
+    pickle.dump(ent_id2idx, f)
+
+with open(rel_id2idx_path, "wb") as f:
+    pickle.dump(rel_id2idx, f)
